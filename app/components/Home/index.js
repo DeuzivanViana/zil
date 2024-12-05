9c25ee2b4db3c50a9e85f0bed3aa47857edb2e54
DeuzivanViana <deuzivanlimaviana@proton.me>
2024-12-05 19:52:48 -0300

    update fonts

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -5,8 +5,7 @@ export const Home = () => {
   return <Layout title={'Home'}>
     <Post
         username={'Deuzivan'}
-        content={'De acordo com a Google, ela está desenvolvendo um computador quântico, capaz de reformular toda nossa tecnologia atual. Uma das principais vantagens da computação quântica, seria possível descobrir novas moléculas, portéinas e consequentimente novos tratamentos para doenças e talvez curas.'}
-        key={0}
+        content={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}        key={0}
     />
   </Layout>
 }
